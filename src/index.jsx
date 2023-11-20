1cfb25b9f80acab98b79cb8520bab5042d3ad295
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-11-20 18:47:50 +0200

    fix persist

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store.js";
 import App from "./components/App/App.jsx";
 
 import "./index.css";
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="/urban-cars-rent">
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
